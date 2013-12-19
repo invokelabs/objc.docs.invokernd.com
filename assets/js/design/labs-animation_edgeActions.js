cab6b36b8208e95392590bdf37b5e2eadbcdbb6b
Alexander <alexander.nguyen@invokelabs.com>
2013-12-19 14:36:06 -0800

    new layout

---
+++ assets/js/design/labs-animation_edgeActions.js
@@ -0,0 +1,58 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",6500,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",15750,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",23500,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",31250,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",38500,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",39000,function(sym,e){sym.stop("design");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",24000,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",31750,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",16500,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",7250,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_1}","click",function(sym,e){sym.stop("design");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_2}","click",function(sym,e){sym.stop("gears");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_3}","click",function(sym,e){sym.stop("social");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_4}","click",function(sym,e){sym.stop("talent");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_5}","click",function(sym,e){sym.stop("strategy");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_1}","mouseover",function(sym,e){var myVariable=sym.getVariable("dot_1");sym.$("dot_1").css({"background-color":"#eb4348"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_1}","mouseout",function(sym,e){var myVariable=sym.getVariable("dot_1");sym.$("dot_1").css({"background-color":"#ffffff"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_2}","mouseover",function(sym,e){var myVariable=sym.getVariable("dot_2");sym.$("dot_2").css({"background-color":"#eb4348"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_3}","mouseover",function(sym,e){var myVariable=sym.getVariable("dot_3");sym.$("dot_3").css({"background-color":"#eb4348"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_4}","mouseover",function(sym,e){var myVariable=sym.getVariable("dot_4");sym.$("dot_4").css({"background-color":"#eb4348"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_5}","mouseover",function(sym,e){var myVariable=sym.getVariable("dot_5");sym.$("dot_5").css({"background-color":"#eb4348"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_2}","mouseout",function(sym,e){var myVariable=sym.getVariable("dot_2");sym.$("dot_2").css({"background-color":"#ffffff"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_3}","mouseout",function(sym,e){var myVariable=sym.getVariable("dot_3");sym.$("dot_3").css({"background-color":"#ffffff"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_4}","mouseout",function(sym,e){var myVariable=sym.getVariable("dot_4");sym.$("dot_4").css({"background-color":"#ffffff"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_dot_5}","mouseout",function(sym,e){var myVariable=sym.getVariable("dot_5");sym.$("dot_5").css({"background-color":"#ffffff"});});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-105205994");
